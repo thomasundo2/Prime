319c64d4472d862296317fe799521dba7226c6b3
pbt-santos <pbt.santos@outlook.com>
2021-03-17 22:00:10 -0400

    Add my take on AST

--- ast.mli
+++ ast.mli
@@ -1,21 +1,37 @@
 (* Create a new operator for assignment and create a new expression*)
 (* sequences of expressions *)
 type operator = Add | Sub | Mul | Div | Semi
+
 type eqsign = Eq
-type expr =
+
+(* type expr =
     Binop of expr * operator * expr
   | Assignmentop of string  * eqsign * expr
   | Getval of string
-  | Lit of int
+  | Lit of int *)
 
 type typ = Int | Lint | Chr | Ring | String | Point | Poly
 
 type bind = typ * string
 
+type expr = 
+    Strlit of string
+  | Lit of int
+  |  Id of string
+  | Call of string * expr list
+  | Noexpr
+
+type stmt = 
+    Block of stmt list
+  | Expr of expr
+  | Return of expr
+
 type func_decl = {
   typ : typ;
   name : string;
   params : bind list;
+  locals : bind list;
+  body : stmt list;
 }
 
 (* This is probably wrong but needs to be added for parser *)
