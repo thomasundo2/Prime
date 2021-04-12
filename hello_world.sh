93477853a2a6f4928bce0b4ad4f7a10c0bc0754c
alex-liebeskind <63989601+alex-liebeskind@users.noreply.github.com>
2021-04-12 12:32:11 -0400

    comparative operators tested

--- hello_world.sh
+++ hello_world.sh
@@ -98,6 +98,7 @@ else
     Test tests/test_ass.pr 2>> $logfile
     Test tests/test_point.pr 2>> $logfile
     Test tests/test_pointadd.pr 2>> $logfile
+    Test tests/test_ops.pr 2>> $logfile
     # Test tests/test_var1_fail.pr 2>> $logfile
 fi
 
