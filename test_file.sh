961495d5efa37c722abc010268868935865ad2fe
nmofficial <nmv.co.uk@gmail.com>
2021-04-18 22:57:26 -0400

    fixed bash script modified tests cases for for/while

--- test_file.sh
+++ test_file.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if [$# -ne 1]; 
-    then echo error
 ./prime.native $1.pr > $1.ll
 llc -relocation-model=pic $1.ll > $1.s
 gcc -o $1.exe $1.s
 ./$1.exe
+rm $1.ll;
+rm $1.s;
+rm $1.exe;
