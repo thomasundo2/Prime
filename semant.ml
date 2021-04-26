89265da7d899b2f8dac290ab1459c50f3ae68f46
nmofficial <nmv.co.uk@gmail.com>
2021-04-25 21:34:59 -0400

    removed test file

--- semant.ml
+++ semant.ml
@@ -160,7 +160,6 @@ let check_function func =
         Point -> (match e2 with
                     "x"     -> (Lint, SAccess(var, 0))
                   | "y"     -> (Lint, SAccess(var, 1))
-                  | "crv" -> (Poly, SAccess(var, 2));
                   | _   -> raise (Failure ("invalid access element " ^ e2 ^ " in "
                                            ^ string_of_expr ex)))
       | _     -> raise (Failure ("cannot access type: " ^ string_of_typ lt
