f20d40c021062fc4b30402509cb0dbb89cbe4b82
nmofficial <nmv.co.uk@gmail.com>
2021-04-26 20:47:02 -0400

    updated test script

--- test_file.sh
+++ test_file.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -x
 ./prime.native $1.pr > $1.ll
 llc -relocation-model=pic $1.ll > $1.s
 cc -c gmpfunc.c
 cc -c structs.c
-gcc -o $1.exe $1.s gmpfunc.o structs.o -lgmp
+cc -c input.c
+gcc -o $1.exe $1.s gmpfunc.o structs.o input.o -lgmp
 ./$1.exe
 rm $1.ll;
 rm $1.s;
 rm $1.exe;
+rm *.o
